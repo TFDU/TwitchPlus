72ee9aa4ad92e2f810722e57db0379c1b092c588
Tianfeng Du <admiralight@gmail.com>
2020-12-25 19:48:47 -0500

    0.0.4

--- src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
+++ src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -15,6 +15,7 @@ import java.util.Set;
 
 import com.laioffer.jupiter.entity.Item;
 import com.laioffer.jupiter.entity.ItemType;
+import com.laioffer.jupiter.entity.User;
 
 public class MySQLConnection {
     private final Connection conn;
@@ -172,5 +173,47 @@ public class MySQLConnection {
             throw new MySQLException("Failed to get favorite game ids from Database");
         }
         return itemMap;
+    }
+    public String verifyLogin(String userId, String password) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+        String name = "";
+        String sql = "SELECT first_name, last_name FROM users WHERE id = ? AND password = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, userId);
+            statement.setString(2, password);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                name = rs.getString("first_name") + " " + rs.getString("last_name");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to verify user id and password from Database");
+        }
+        return name;
+    }
+
+    public boolean addUser(User user) throws MySQLException {
+        if (conn == null) {
+            System.err.println("DB connection failed");
+            throw new MySQLException("Failed to connect to Database");
+        }
+
+        String sql = "INSERT IGNORE INTO users VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, user.getUserId());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getFirstName());
+            statement.setString(4, user.getLastName());
+
+            return statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new MySQLException("Failed to get user information from Database");
+        }
     }
 }
