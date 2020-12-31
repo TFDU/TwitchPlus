1b93dfd11e83807eb08a10568d18524960293e88
Tianfeng Du <admiralight@gmail.com>
2020-12-31 01:13:44 -0500

    0.0.6

--- webFront/twitchplus/src/App.js
+++ webFront/twitchplus/src/App.js
@@ -1,27 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
-  );
+import React from 'react';
+import { Button, Layout, message } from 'antd';
+import Login from './components/Login';
+import Register from './components/Register';
+import { logout } from './utils';
+ 
+const { Header, Content, Sider } = Layout;
+ 
+class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+ 
+  signinOnSuccess = () => {
+    this.setState({
+      loggedIn: true
+    })
+  }
+ 
+  signoutOnClick = () => {
+    logout()
+      .then(() => {
+        this.setState({
+          loggedIn: false
+        })
+        message.success(`Successfull signed out`);
+      }).catch((err) => {
+        message.error(err.message);
+      })
+  }
+ 
+  render = () => (
+    <Layout>
+      <Header>
+        {
+          this.state.loggedIn ? 
+          <Button shape="round" onClick={this.signoutOnClick}>
+            Logout</Button> :
+          (
+            <>
+              <Login onSuccess={this.signinOnSuccess} />
+              <Register />
+            </>
+          )
+        }
+      </Header>
+      <Layout>
+        <Sider width={300} className="site-layout-background">
+          {'Sider'}
+        </Sider>
+        <Layout style={{ padding: '24px' }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              height: 800,
+              overflow: 'auto'
+            }}
+          >
+            {'Home'} 
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  )
 }
-
+ 
 export default App;
